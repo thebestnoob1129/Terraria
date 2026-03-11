a57fac1bad0f7e26119c9e6710e2d949522e6f53
TheBestNoob <73605149+thebestnoob1129@users.noreply.github.com>
2026-03-11 12:24:58 -0400

    first commit

---
+++ src/Terraria.cpp
@@ -0,0 +1,20 @@
+﻿// Terraria.cpp : Defines the entry point for the application.
+//
+
+#include "Terraria.h"
+
+using namespace std;
+
+// const char *PlayerData = ResourcePath "./playerData:
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+
+#if PRODUCITON_BUILD == 0
+	std::cout << "Production Build \n";
+#endif
+
+
+	return 0;
+}
